0e719152e4ac7aaca49760d5a924a6cdfc609d51
Nara <narachhuo@gmail.com>
2025-03-21 21:12:53 -0500

    Removed register in sign in page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -59,9 +59,7 @@ const Login = () => {
           />
           <button type="submit">Login</button>
         </form>
-        <p>
-          Don't have an account? <Link to="/register">Sign up</Link>
-        </p>
+        
       </div>
     </div>
   );
